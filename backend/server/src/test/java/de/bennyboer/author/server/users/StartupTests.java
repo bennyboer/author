d62595db5836e8f5e82db8368a15e2ec8dde1a9a
bennyboer <barny.eder@gmail.com>
2024-01-04 13:46:21 +0100

    Testing that default user is not created when already there

--- backend/server/src/test/java/de/bennyboer/author/server/users/StartupTests.java
+++ backend/server/src/test/java/de/bennyboer/author/server/users/StartupTests.java
@@ -1,6 +1,9 @@
 package de.bennyboer.author.server.users;
 
+import de.bennyboer.author.common.UserId;
 import de.bennyboer.author.server.users.api.responses.LoginUserResponse;
+import de.bennyboer.author.server.users.persistence.lookup.LookupUser;
+import de.bennyboer.author.user.UserName;
 import io.javalin.testtools.JavalinTest;
 import org.junit.jupiter.api.Test;
 
@@ -22,10 +25,15 @@ public class StartupTests extends UsersModuleTests {
 
     @Test
     void shouldNotCreateDefaultUserOnStartupWhenAlreadyHavingPersistentUsers() {
-        // TODO Modify TestUserLookupRepo to return a user on startup that is not the default user!
+        // given: there is already a user in the database
+        userLookupRepo.update(LookupUser.of(UserId.create(), UserName.of("TestUser"))).block();
 
         JavalinTest.test(javalin, (server, client) -> {
-            assertThat(false).isTrue(); // TODO Remove this line and implement the test!
+            // when: server started up
+            Thread.sleep(500);
+
+            // then: the default user has not been created
+            assertThat(userLookupRepo.findUserIdByName(UserName.of("default")).block()).isNull();
         });
     }
 
